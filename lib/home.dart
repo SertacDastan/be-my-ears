56b3f363c6eafadd55229e0ed9702c2c810f6838
Sertaç Daştan <sertacdastan1@gmail.com>
2022-12-20 20:11:22 +0300

    speech to text eklendi

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,29 +11,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _hasSpeech = false;
+  bool isEnabled = false;
+  String _currentLocaleId = 'tr-TR';
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String lastWords = '';
+  String lastError = '';
+  String lastStatus = '';
+  bool _logEvents = true;
+
+  final SpeechToText speech = SpeechToText();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    initSpeechState();
+    super.initState();
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    _logEvent('Received error status: $error, listening: ${speech.isListening}');
+    setState(() {
+      lastError = '${error.errorMsg} - ${error.permanent}';
+    });
+  }
+
+  void statusListener(String status) {
+    _logEvent('Received listener status: $status, listening: ${speech.isListening}');
+    setState(() {
+      lastStatus = '$status';
+    });
+  }
+
+  void _logEvent(String eventDescription) {
+    if (_logEvents) {
+      var eventTime = DateTime.now().toIso8601String();
+      print('$eventTime $eventDescription');
+    }
+  }
+
+  void _switchLang(selectedVal) {
+    setState(() {
+      _currentLocaleId = selectedVal;
+    });
+    print(selectedVal);
+  }
+
+  Future<void> initSpeechState() async {
+    _logEvent('Initialize');
+
+    try {
+      var hasSpeech = await speech.initialize(
+        onError: errorListener,
+        onStatus: statusListener,
+        debugLogging: false,
+      );
+      if (!mounted) return;
+
+      setState(() {
+        _hasSpeech = hasSpeech;
+      });
+    } catch (e) {
+      setState(() {
+        _hasSpeech = false;
+      });
+    }
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    _logEvent('Result listener final: ${result.finalResult}, words: ${result.recognizedWords}');
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  void startListening() {
+    _logEvent('start listening');
+    lastWords = '';
+    lastError = '';
+    speech.listen(
+      onResult: resultListener,
+      pauseFor: const Duration(seconds: 10),
+      partialResults: true,
+      localeId: _currentLocaleId,
+      cancelOnError: true,
+      listenMode: ListenMode.confirmation,
+    );
+    setState(() {});
+  }
+
+  void stopListening() {
+    _logEvent('stop');
+    speech.stop();
+    setState(() {
+      level = 0.0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Be My Ears"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: [
-                  const Text(
-                    'Merhaba. Nasılsınız...',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ],
-              ),
-              Image.asset("lib/assets/download.png"),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.mic)),
-            ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15, top: 15.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  lastWords == '' && isEnabled ? "Konuşmaya başlamak için butona basın..." : lastWords,
+                  style: TextStyle(fontSize: 25),
+                ),
+                // Image.asset("lib/assets/download.png"),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: isEnabled ? Colors.blue : Colors.blue.shade200,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                      minimumSize: const Size(60, 60),
+                      padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 8),
+                    ),
+                    onPressed: () {
+                      isEnabled = !isEnabled;
+                      if (isEnabled)
+                        stopListening();
+                      else
+                        startListening();
+                    },
+                    child: const Icon(Icons.mic)),
+              ],
+            ),
           ),
         ),
       ),
